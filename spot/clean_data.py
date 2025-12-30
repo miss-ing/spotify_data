abf904d2b7d130648984776588fb6c29103d2df3
miss-ing <emingslw@gmail.com>
2025-12-30 18:54:00 +0000

    dataframe cleaning and removing notebooks

--- spot/clean_data.py
+++ spot/clean_data.py
@@ -1,5 +1,28 @@
-
+import pandas as pd
 #df['track_name'] = df['track_name'].astype(str).str.strip().str.lower()
 #df['artist_name']= df['artist_name'].astype(str).str.strip().str.lower()
 #df['album_name']= df['album_name'].astype(str).str.strip().str.lower()
 
+
+
+def many_to_many(uri_info : pd.DataFrame, *,column :str, key: str | None = None) -> pd.DataFrame:
+    #create new df with two manys 
+    mmdf = uri_info[['id', column]].copy()
+
+    if key != None:
+        mmdf[column]= mmdf[column].apply(
+            lambda x: [y[key] for y in x])
+    
+    mmdf= mmdf.explode(column)
+
+    return mmdf
+
+def dictcolumn(column : str, key: str) -> str:
+    
+    new_column = column.apply(
+    lambda d: d.get(key) 
+    if isinstance(d,dict) and key in d 
+    else None)
+
+
+    return new_column
